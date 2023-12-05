cce41817f7663330445025a87d1ba3630d811082
chenhaojie <a18520145493@163.com>
2023-12-05 11:10:01 +0800

    feat: 仪表盘流式布局并共享页面

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,22 +1,38 @@
+/*
+ * @Author: chenhaojie a18520145493@163.com
+ * @Date: 2023-11-27 16:10:08
+ * @LastEditors: chenhaojie a18520145493@163.com
+ * @LastEditTime: 2023-12-05 10:53:47
+ * @FilePath: \text-dashboard\app\dashboard\(overview)\page.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '../ui/dashboard/revenue-chart';
-import LatestInvoices from '../ui/dashboard/latest-invoices';
+import RevenueChart from '../../ui/dashboard/revenue-chart';
+import LatestInvoices from '../../ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
+import { unstable_cache as noCache } from 'next/cache';
 import {
   fetchRevenue,
   fetchLatestInvoices,
   fetchCardData,
 } from '@/app/lib/data';
 
+const getRevenueData = noCache(async () => fetchRevenue(), ['fetchRevenue']);
+const getLatestInvoices = noCache(
+  async () => fetchLatestInvoices(),
+  ['fetchLatestInvoices'],
+);
+const getCardData = noCache(async () => fetchCardData(), ['fetchCardData']);
+
 export default async function Page() {
-  const revenueData = await fetchRevenue();
-  const latestInvoicesData = await fetchLatestInvoices();
+  const revenueData = await getRevenueData();
+  const latestInvoicesData = await getLatestInvoices();
   const {
     totalPaidInvoices,
     totalPendingInvoices,
     numberOfCustomers,
     numberOfInvoices,
-  } = await fetchCardData();
+  } = await getCardData();
   return (
     <main className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
       <h1>Dashboard</h1>
