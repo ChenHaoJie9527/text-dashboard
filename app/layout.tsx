64be076675f45eae8f899d853a403fd49235ed1f
chenhaojie <a18520145493@163.com>
2023-12-11 15:07:13 +0800

    feat: 添加页面元数据信息

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css';
 import { inter } from './ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '仪表盘',
+  description: '这是next.js实现仪表盘',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
